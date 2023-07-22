11732b1a6f2331e4c8ff325712384f1a65556642
Den Williams <dennis.williams@loke.com.au>
2023-07-22 21:36:47 +1000

    fix paths

--- sea.sh
+++ sea.sh
@@ -3,7 +3,7 @@ nodejs_version="${NODEJS_VERSION:-20.4.0}"
 NODE_DIR=".sea/node"
 
 mkdir -p "${NODE_DIR}"
-npx esbuild src/main.ts --bundle --platform=node --target=node20.4 --outfile=.sea/main.js
+npx esbuild main.js --bundle --platform=node --target=node20.4 --outfile=.sea/main.js
 echo '{ "main": ".sea/main.js", "output": ".sea/prep.blob", "disableExperimentalSEAWarning": true }' > .sea/config.json
 node --experimental-sea-config .sea/config.json
 
